bd0071f24d05fe070fa838ce3957c7388abf56a2
Olivira-27 <oliviraadi@gmail.com>
2021-01-14 20:11:46 +0700

    Pratikum 3

---
+++ src/pra3/Pra3.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pra3;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Pra3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a;
+        for (a = 0; a < 2; a++) {
+            System.out.println("");
+        } 
+        int b = 4;
+        for (int z = 0; z < 6; z++) {
+            System.out.print("@");
+        }
+        System.out.println("");
+        System.out.println("   @");
+        System.out.println("  @");
+        System.out.println(" @");
+        for (int z = 0; z < 6; z++) {
+            System.out.print("@");
+        }
+        System.out.println("");
+    }
+ 
+}
+    
+    
+
